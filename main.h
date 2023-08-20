510182e59c002b01b2dba5105174c3f8ae56e487
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-19 18:57:34 -0700

    task-0_second-upload

--- main.h
+++ main.h
@@ -3,6 +3,10 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
 int print_char(char c);
