1606e164368ac9556f0e1127224ad84303e1a183
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-20 19:44:07 -0700

    functions needed

--- functions.c
+++ functions.c
@@ -1,20 +1,24 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 /**
  * _putchar - writes a character to the standard output
  * @c: the character to be written
+ *
  * Return: on success, 1 is returned. On error, -1 is returned,
  * and errno is set appropriately.
-*/
+ */
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
 /**
  * _print_number - prints an integer using recursion
  * @n: the integer to be printed
+ *
  * Return: the number of characters printed
-*/
-
+ */
 int _print_number(int n)
 {
 	int len = 0;
@@ -25,12 +29,13 @@ int _print_number(int n)
 
 	return (len);
 }
-/*handele cases*/
+
 /**
  * handle_char - handles the %c conversion specifier
  * @args: va_list containing the argument
+ *
  * Return: number of characters printed
-*/
+ */
 int handle_char(va_list args)
 {
 	char c = va_arg(args, int);
@@ -38,35 +43,37 @@ int handle_char(va_list args)
 	_putchar(c);
 	return (1); /*Return the number of characters printed*/
 }
+
 /**
  * handle_string - handles the %s conversion specifier
  * @args: va_list containing the argument
+ *
  * Return: number of characters printed
-*/
+ */
 int handle_string(va_list args)
 {
 	char *str = va_arg(args, char *);
-	/*Handle the NULL string case*/
-	if (!str)
-	{
-		_putchar(' ');
-		return (1);
-	}
 	int printed_chars = 0;
 
-	while (*str)
+	if (str)
 	{
-		_putchar(*str);
-		str++;
-		printed_chars++;
+		while (*str)
+		{
+			_putchar(*str);
+			str++;
+			printed_chars++;
+		}
 	}
+
 	return (printed_chars);
 }
+
 /**
  * handle_decimal - handles the %d and %i conversion specifiers
  * @args: va_list containing the argument
+ *
  * Return: number of characters printed
-*/
+ */
 int handle_decimal(va_list args)
 {
 	int num = va_arg(args, int);
