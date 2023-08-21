7d5709473cad95da202fd2abfa1c68555835bea7
Mery9yuka <benhaida.maryem@gmail.com>
2023-08-20 18:07:07 -0700

    functions

--- functions.c
+++ functions.c
@@ -1,44 +1,84 @@
 #include "main.h"
-#include <unistd.h>
-
 /**
- * print_char - prints a character
- * @c: character to print
- *
- * Return: The number of characters printed
+ * _putchar - writes a character to the standard output
+ * @c: the character to be written
+ * Return: on success, 1 is returned. On error, -1 is returned,
+ * and errno is set appropriately.
 */
-int print_char(char c)
+int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
 /**
- * print_string - prints a string
- * @str: string to print
- *
- * Return: The number of characters printed
+ * _print_number - prints an integer using recursion
+ * @n: the integer to be printed
+ * Return: the number of characters printed
 */
-int print_string(char *str)
+
+int _print_number(int n)
 {
 	int len = 0;
 
-	if (str)
-	{
-		while (str[len])
-			len += print_char(str[len]);
-	}
-	else
-		len += print_string("(null)");
+	if (n / 10 != 0)
+		len += _print_number(n / 10);
+	len += _putchar((n % 10) + '0');
 
 	return (len);
 }
-
+/*handele cases*/
 /**
- * print_percent - prints a percent character '%'
- *
- * Return: The number of characters printed (always 1)
+ * handle_char - handles the %c conversion specifier
+ * @args: va_list containing the argument
+ * Return: number of characters printed
 */
-int print_percent(void)
+int handle_char(va_list args)
 {
-	return (print_char('%'));
+	char c = va_arg(args, int);
+
+	_putchar(c);
+	return (1); /*Return the number of characters printed*/
+}
+/**
+ * handle_string - handles the %s conversion specifier
+ * @args: va_list containing the argument
+ * Return: number of characters printed
+*/
+int handle_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	/*Handle the NULL string case*/
+	if (!str)
+	{
+		_putchar(' ');
+		return (1);
+	}
+	int printed_chars = 0;
+
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+		printed_chars++;
+	}
+	return (printed_chars);
+}
+/**
+ * handle_decimal - handles the %d and %i conversion specifiers
+ * @args: va_list containing the argument
+ * Return: number of characters printed
+*/
+int handle_decimal(va_list args)
+{
+	int num = va_arg(args, int);
+	int len = 0;
+
+	if (num < 0)
+	{
+		_putchar('-');
+		num = -num;
+		len++;
+	}
+
+	len += _print_number(num);
+	return (len);
 }
---
+++ functions1.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * handle_percent - handles the %% conversion specifier
+ *
+ * Return: number of characters printed
+ */
+int handle_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
